7a841e7542903690bb4787ab3e05c95f76c97b17
jiro4989 <jiroron666@gmail.com>
2019-02-17 22:31:35 +0900

    :green_heart:テストコード追加

--- board_test.go
+++ board_test.go
@@ -1,28 +1,57 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFetchRow(t *testing.T) {
+func TestFetchBlock(t *testing.T) {
 	board := Board{
-		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
-		{1, 1, 1, 1, 0, 0, 0, 1, 1, 1},
-		{1, 1, 1, 0, 1, 0, 0, 1, 1, 1},
-		{1, 1, 1, 0, 0, 1, 0, 1, 1, 1},
-		{1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	}
 
-	assert.Equal(t, Row{0, 0, 0, 0}, board.fetchRow(0), "先頭行を取得")
-	assert.Equal(t, Row{1, 0, 0, 0}, board.fetchRow(1), "1行目取得")
-	assert.Equal(t, Row{0, 1, 0, 0}, board.fetchRow(2), "2行目取得")
-	assert.Equal(t, Row{0, 0, 1, 0}, board.fetchRow(3), "3行目取得")
-	assert.Equal(t, Row{0, 0, 0, 1}, board.fetchRow(4), "4行目取得")
-	assert.Equal(t, Row{1, 1, 1, 1}, board.fetchRow(5), "5行目取得")
+	assert.Equal(t, Block{
+		{1, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1},
+	}, board.fetchBlock(3, 0), "取得")
+}
+
+func TestDeleteRows(t *testing.T) {
+	board := Board{
+		{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	}
+
+	board.deleteRows()
+
+	ret := Board{
+		{1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	}
+	assert.Equal(t, ret, board, "削除によってスライドする")
+	board.deleteRows()
+	assert.Equal(t, ret, board, "これ以上削除できない")
 }
 
 func TestDeleteRow(t *testing.T) {
@@ -45,8 +74,51 @@ func TestDeleteRow(t *testing.T) {
 		{1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1},
 		{1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1},
 	}, board, "削除によってスライドする")
+}
 
-	for _, row := range board {
-		fmt.Println(row)
+func TestFetchRow(t *testing.T) {
+	board := Board{
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 1, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 1, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 1, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	}
+
+	assert.Equal(t, Row{0, 0, 0, 0}, board.fetchRow(0), "先頭行を取得")
+	assert.Equal(t, Row{1, 0, 0, 0}, board.fetchRow(1), "1行目取得")
+	assert.Equal(t, Row{0, 1, 0, 0}, board.fetchRow(2), "2行目取得")
+	assert.Equal(t, Row{0, 0, 1, 0}, board.fetchRow(3), "3行目取得")
+	assert.Equal(t, Row{0, 0, 0, 1}, board.fetchRow(4), "4行目取得")
+	assert.Equal(t, Row{1, 1, 1, 1}, board.fetchRow(5), "5行目取得")
+
+}
+
+func TestSetMino(t *testing.T) {
+	board := Board{
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	}
+
+	board.setMino(Mino{x: 3})
+
+	assert.Equal(t, Board{
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 2, 2, 0, 1, 1, 1},
+		{1, 1, 1, 2, 2, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	}, board, "セットされる")
+}
+
+func TestIsDeletable(t *testing.T) {
+	assert.Equal(t, true, Row{1, 1, 1, 1}.isDeletable(), "削除可")
+	assert.Equal(t, false, Row{1, 0, 1, 1}.isDeletable(), "削除不可")
+	assert.Equal(t, false, Row{0, 0, 0, 0}.isDeletable(), "削除不可")
 }
--- mino_test.go
+++ mino_test.go
@@ -57,3 +57,85 @@ func TestIsOverlap(t *testing.T) {
 	}), "全部ブロックが埋まってるときはtrue")
 
 }
+
+func TestGetBlock(t *testing.T) {
+	mino := Mino{minoIndex: 0}
+	assert.Equal(t, Block{
+		{0, 0, 0, 0},
+		{0, 2, 2, 0},
+		{2, 2, 0, 0},
+		{0, 0, 0, 0},
+	}, mino.getBlock())
+}
+
+var board = Board{
+	{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+	{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+	{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+	{1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+}
+
+func TestCanMoveRight(t *testing.T) {
+	mino := Mino{minoIndex: 0, x: 3, y: 0}
+	assert.Equal(t, true, mino.canMoveRight(board))
+	mino.x++
+	assert.Equal(t, false, mino.canMoveRight(board))
+	mino.x = 111
+	assert.Equal(t, false, mino.canMoveRight(board))
+}
+
+func TestCanMoveLeft(t *testing.T) {
+	mino := Mino{minoIndex: 0, x: 3, y: 0}
+	assert.Equal(t, false, mino.canMoveLeft(board))
+	mino.x++
+	assert.Equal(t, true, mino.canMoveLeft(board))
+	mino.x = -1
+	assert.Equal(t, false, mino.canMoveLeft(board))
+}
+
+func TestCanMoveDown(t *testing.T) {
+	mino := Mino{minoIndex: 0, x: 3, y: 0}
+	assert.Equal(t, true, mino.canMoveDown(board))
+	mino.y = 1
+	assert.Equal(t, false, mino.canMoveDown(board))
+	mino.y = 3
+	assert.Equal(t, false, mino.canMoveDown(board))
+	mino.y = 111
+	assert.Equal(t, false, mino.canMoveDown(board))
+}
+
+func TestMove(t *testing.T) {
+	mino := Mino{x: 0, y: 0}
+
+	mino.moveRight()
+	assert.Equal(t, 1, mino.x)
+
+	mino.moveLeft()
+	assert.Equal(t, 0, mino.x)
+
+	mino.moveDown()
+	assert.Equal(t, 1, mino.y)
+}
+
+func TestRotateRight(t *testing.T) {
+	mino := Mino{}
+
+	mino.rotateRight()
+	assert.Equal(t, 1, mino.rotateIndex)
+
+	mino.rotateRight()
+	assert.Equal(t, 0, mino.rotateIndex)
+}
+
+func TestRotateLeft(t *testing.T) {
+	mino := Mino{}
+
+	mino.rotateLeft()
+	assert.Equal(t, 1, mino.rotateIndex)
+
+	mino.rotateLeft()
+	assert.Equal(t, 0, mino.rotateIndex)
+}
